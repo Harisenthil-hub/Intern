be88cae1586e6b8644ddff1e6f57a89bcf515124
AnirudhVisvesvaran <AnirudhVisvesvaran@gmail.com>
2026-04-29 18:39:26 +0530

    Revert "Added frontend files"

--- src/components/common/Badge.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/common/Badge.jsx
-export default function Badge({ status }) {
-  const isDraft = status === "draft";
-  return (
-    <span className={`badge ${isDraft ? "badge-draft" : "badge-posted"}`}>
-      {isDraft ? "Draft" : "Posted"}
-    </span>
-  );
-}
